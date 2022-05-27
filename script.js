0fbee84bbe12596d81fd3ba7803a895a11b60f9d
ldwitts <ldwitts@gmail.com>
2022-05-26 23:02:49 -0500

    Add reset button and random color on mouseover

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const grid = document.querySelector('.grid');
+const resetButton = document.querySelector('.reset-button');
 
-numberOfCols = 10;
+numberOfCols = 50;
 numberOfItems = numberOfCols ** 2;
 
 grid.style.setProperty('grid-template-columns', `repeat(${numberOfCols}, 1fr)`);
 
 for (let i = 0; i < numberOfItems; i++) {
   const div = document.createElement('div');
-  grid.appendChild(div);
+
   div.className = 'grid-item';
+  div.addEventListener('mouseover', function (event) {
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    event.target.style.backgroundColor = `#${randomColor}`;
+  });
+  grid.appendChild(div);
 }
+
+resetButton.addEventListener('click', function () {
+  const gridItems = document.querySelectorAll('.grid-item');
+  gridItems.forEach((element) => {
+    element.style.backgroundColor = 'transparent';
+  });
+});
